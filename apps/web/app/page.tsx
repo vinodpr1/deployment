430e2abb7f29af25c8f9cc07dc53d2e3f7537511
Vinod Prajapati <vinodpr737947@gmail.com>
2025-03-30 11:18:04 +0530

    deployment  commit test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -7,7 +7,8 @@ export default async function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-      
+         
+         Hello deployment server
 
         <div className={styles.ctas}>
           <a
